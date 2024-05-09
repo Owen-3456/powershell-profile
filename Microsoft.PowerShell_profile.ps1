6e696f74e849d1eb00d2e21ef1647af95c44cc14
Owen <78411938+Owen-3456@users.noreply.github.com>
2024-05-09 17:53:09 +0100

    Added hastebin upload function to profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,27 @@
+# Automatically uploads a file to bin.christitus.com
+# Usage: hb "C:\Path\To\File.txt"
+function hb {
+ if ($args.Length -eq 0) {
+ Write-Error "No file path specified."
+ return
+ }
+
+ $FilePath = $args[0]
+
+ if (Test-Path $FilePath) {
+ $Content = Get-Content $FilePath -Raw
+ } else {
+ Write-Error "File path does not exist."
+ return
+ }
+
+ $uri = "http://bin.christitus.com/documents"
+ try {
+ $response = Invoke-RestMethod -Uri $uri -Method Post -Body $Content -ErrorAction Stop
+ $hasteKey = $response.key
+ $url = "http://bin.christitus.com/$hasteKey"
+ Write-Output $url
+ } catch {
+ Write-Error "Failed to upload the document. Error: $_"
+ }
+}
