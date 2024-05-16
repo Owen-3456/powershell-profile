a50b144186a1ebcfb1694236182a4fd5785d41d6
Owen <78411938+Owen-3456@users.noreply.github.com>
2024-05-16 19:29:01 +0100

    Added messages to setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,5 @@
+Write-Host "Loading Owen3456's Profile" -ForegroundColor Cyan
 $profileUrl = "https://raw.githubusercontent.com/Owen-3456/powershell-profile/main/Microsoft.PowerShell_profile.ps1"
 $profilePath = "$HOME\Documents\PowerShell"
 Invoke-WebRequest -Uri $profileUrl -OutFile $profilePath
+Write-Host "Loaded Owen3456's Profile" -ForegroundColor Green
