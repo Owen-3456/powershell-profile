7fc66710be66315906c096264c3572573f687e9c
Owen <78411938+Owen-3456@users.noreply.github.com>
2024-07-01 23:21:57 +0100

    Added fastfetch dir check, changed variable name

--- install.ps1
+++ install.ps1
@@ -4,20 +4,24 @@ try {
     winget install Microsoft.PowerShell JanDeDobbeleer.OhMyPosh ajeetdsouza.zoxide fzf Fastfetch-cli.Fastfetch gerardog.gsudo
 
     # Downloads profile and saves to file path
-    $profileUrl = "https://raw.githubusercontent.com/Owen-3456/powershell-profile/main/Microsoft.PowerShell_profile.ps1"
-    $profilePath = "$HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
-    if (-not (Test-Path -Path $profilePath)) {
-        New-Item -ItemType File -Path $profilePath -Force | Out-Null
+    $profile_Url = "https://raw.githubusercontent.com/Owen-3456/powershell-profile/main/Microsoft.PowerShell_profile.ps1"
+    $profile_Path = "$HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
+    if (-not (Test-Path -Path $profile_Path)) {
+        New-Item -ItemType File -Path $profile_Path -Force | Out-Null
     }
-    Invoke-WebRequest -Uri $profileUrl -OutFile $profilePath
+    Invoke-WebRequest -Uri $profile_Url -OutFile $profile_Path
     
     # Downloads fastfetch config and saves to file path
-    $fastfetchconfigUrl = "https://raw.githubusercontent.com/Owen-3456/powershell-profile/main/config.jsonc"
-    $fastfetchconfigPath = "$HOME\fastfetch\config.jsonc"
-    if (-not (Test-Path -Path $fastfetchconfigPath)) {
-        New-Item -ItemType File -Path $fastfetchconfigPath | Out-Null
+    $fastfetch_Config_Url = "https://raw.githubusercontent.com/Owen-3456/powershell-profile/main/config.jsonc"
+    $fastfetch_Config_Path = "$HOME\fastfetch\config.jsonc"
+    if (-not (Test-Path -Path $fastfetch_Config_Path)) {
+        $fastfetch_Directory_Path = Split-Path -Path $fastfetch_Config_Path -Parent
+        if (-not (Test-Path -Path $fastfetch_Directory_Path)) {
+            New-Item -ItemType Directory -Path $fastfetch_Directory_Path | Out-Null
+        }
+        New-Item -ItemType File -Path $fastfetch_Config_Path | Out-Null
     }
-    Invoke-WebRequest -Uri $fastfetchconfigUrl -OutFile $fastfetchconfigPath
+    Invoke-WebRequest -Uri $fastfetch_Config_Url -OutFile $fastfetch_Config_Path
 
     # Output completion message
     Write-Host "Loaded Owen3456's Profile" -ForegroundColor Green
